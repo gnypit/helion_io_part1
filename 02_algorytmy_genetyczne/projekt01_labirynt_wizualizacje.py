6d25a0964b475e59c5db82052bd483a35132826e
Jakub T. Gnyp <gnyp.jakub@gmail.com>
2024-07-17 20:51:15 +0200

    Utworzyłem dodatkowy plik z funkcją do tworzenia GIFa i PNG z wizualizacjami podanej trasy, dla danego labiryntu i kodowania ruchów

---
+++ 02_algorytmy_genetyczne/projekt01_labirynt_wizualizacje.py
@@ -0,0 +1,88 @@
+"""Skrypt przygotowany w ramach kursu z inteligencji obliczeniowej dla wydawnictwa Helios
+Jakub T. Gnyp, gnyp.jakub@gmail.com
+Projekt nr 1: Labirynt — funkcje do wizualizacji
+Wymagany program: ImageMagick https://www.techspot.com/downloads/5515-imagemagick.html
+"""
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.animation as animation
+
+
+def see_route(labyrinth, moves_mapping, steps, gif_filename='labirynt.gif', summary_filename='labirynt_summary.png'):
+    """Funkcja przyjmująca na wejściu macierz reprezentującą labirynt (labyrinth), słownik dopasowujący kod ruchu
+    do zmiany odpowiednich współrzędnych"""
+    start_pos = (1, 1)
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    # Rysowanie labiryntu
+    for i in range(len(labyrinth)):
+        for j in range(len(labyrinth[i])):
+            if labyrinth[i, j] == 1:
+                rect = patches.Rectangle(
+                    (j, i), 1, 1, linewidth=1, edgecolor='black', facecolor='black'
+                )
+                ax.add_patch(rect)
+            else:
+                rect = patches.Rectangle(
+                    (j, i), 1, 1, linewidth=1, edgecolor='grey', facecolor='white'
+                )
+                ax.add_patch(rect)
+
+    path_x, path_y = [start_pos[1] + 0.5], [start_pos[0] + 0.5]
+    pos = list(start_pos)
+
+    # Tworzenie trasy na podstawie kroków
+    for step in steps:
+        move = moves_mapping[step]
+        pos[0] += move[0]
+        pos[1] += move[1]
+        path_x.append(pos[1] + 0.5)
+        path_y.append(pos[0] + 0.5)
+
+    # Funkcja do aktualizacji animacji
+    def update(num, path_x, path_y, line):
+        line.set_data(path_x[:num], path_y[:num])
+        return line,
+
+    # Inicjalizacja linii trasy
+    line, = ax.plot([], [], lw=2, color='red')
+
+    # Animacja
+    ani = animation.FuncAnimation(fig, update, frames=len(path_x), fargs=[path_x, path_y, line], interval=200,
+                                  blit=True)
+
+    # Ustawienia osi
+    plt.xlim(0, 12)
+    plt.ylim(0, 12)
+    plt.gca().invert_yaxis()
+
+    # Zapisanie animacji jako GIF
+    ani.save(gif_filename, writer='imagemagick')
+    print(f"Wygenerowano animację trasy przez labirynt")
+
+    # Rysowanie całej trasy na grafice PNG
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    # Rysowanie labiryntu
+    for i in range(len(labyrinth)):
+        for j in range(len(labyrinth[i])):
+            if labyrinth[i, j] == 1:
+                rect = patches.Rectangle((j, i), 1, 1, linewidth=1, edgecolor='black', facecolor='black')
+                ax.add_patch(rect)
+            else:
+                rect = patches.Rectangle((j, i), 1, 1, linewidth=1, edgecolor='grey', facecolor='white')
+                ax.add_patch(rect)
+
+    ax.plot(path_x, path_y, lw=2, color='red')
+
+    # Ustawienia osi
+    plt.xlim(0, 12)
+    plt.ylim(0, 12)
+    plt.gca().invert_yaxis()
+
+    # Zapisanie obrazu jako PNG
+    plt.savefig(summary_filename)
+    plt.close()
+    print(f"Wykonano grafikę z całą trasą przez labirynt")
