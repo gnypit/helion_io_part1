b751b20f4edb69b3cf88a023a8740076e171317a
Jakub T. Gnyp <gnyp.jakub@gmail.com>
2024-07-17 21:11:01 +0200

    Dodałem ostatnie objaśniające komentarze i usunąłem relikty, tj. w funkcje see_route implementowana jest teraz funkcja draw_labyrinth

--- 02_algorytmy_genetyczne/projekt01_labirynt_wizualizacje.py
+++ 02_algorytmy_genetyczne/projekt01_labirynt_wizualizacje.py
@@ -46,42 +46,30 @@ def see_route(labyrinth: np.ndarray, moves_mapping: dict, steps: list,
     """
     start_pos = (1, 1)
     fig, ax = plt.subplots()
-    draw_labyrinth()
 
-    """Rysowanie labiryntu"""
-    for i in range(len(labyrinth)):
-        for j in range(len(labyrinth[i])):
-            if labyrinth[i, j] == 1:
-                rect = patches.Rectangle(
-                    (j, i), 1, 1, linewidth=1, edgecolor='black', facecolor='black'
-                )
-                ax.add_patch(rect)
-            else:
-                rect = patches.Rectangle(
-                    (j, i), 1, 1, linewidth=1, edgecolor='grey', facecolor='white'
-                )
-                ax.add_patch(rect)
-
-    path_x, path_y = [start_pos[1] + 0.5], [start_pos[0] + 0.5]
-    pos = list(start_pos)
+    """Rysowanie pustego labiryntu"""
+    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)
 
     """Tworzenie trasy na podstawie kroków"""
+    path_x, path_y = [start_pos[1] + 0.5], [start_pos[0] + 0.5]  # ścieżka zaczyna się w środku pola wejścia, stąd 0.5
+    pos = list(start_pos)
     for step in steps:
         move = moves_mapping[step]
         pos[0] += move[0]
         pos[1] += move[1]
-        path_x.append(pos[1] + 0.5)
-        path_y.append(pos[0] + 0.5)
+        path_x.append(pos[1] + 0.5)  # przesunięcie o 0.5, żeby linie były "wycentrowane", a nie wzdłuż krawędzi pól
+        path_y.append(pos[0] + 0.5)  # przesunięcie o 0.5, żeby linie były "wycentrowane", a nie wzdłuż krawędzi pól
 
-    """Funkcja do aktualizacji animacji"""
-    def update(num, path_x, path_y, line):
+    def update(num, path_x, path_y, line):  # wewnątrz funkcji można tworzyć "lokalną" funkcję
+        """Funkcja do aktualizacji animacji"""
         line.set_data(path_x[:num], path_y[:num])
         return line,
 
     """Inicjalizacja linii trasy"""
     line, = ax.plot([], [], lw=2, color='red')
 
-    """Animacja"""
+    """Animacja trasy wzdłuż odpowiednich współrzędnych z zadanym interwałem, określającym jak szybko zmieniają się
+    klatki w końcowym GIFie."""
     ani = animation.FuncAnimation(fig, update, frames=len(path_x), fargs=[path_x, path_y, line], interval=200,
                                   blit=True)
 
@@ -95,20 +83,9 @@ def see_route(labyrinth: np.ndarray, moves_mapping: dict, steps: list,
     print(f"Wygenerowano animację trasy przez labirynt")
 
     """Rysowanie całej trasy na grafice PNG"""
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-
-    # Rysowanie labiryntu
-    for i in range(len(labyrinth)):
-        for j in range(len(labyrinth[i])):
-            if labyrinth[i, j] == 1:
-                rect = patches.Rectangle((j, i), 1, 1, linewidth=1, edgecolor='black', facecolor='black')
-                ax.add_patch(rect)
-            else:
-                rect = patches.Rectangle((j, i), 1, 1, linewidth=1, edgecolor='grey', facecolor='white')
-                ax.add_patch(rect)
-
-    ax.plot(path_x, path_y, lw=2, color='red')
+    fig, ax = plt.subplots()  # wykorzystuję ponownie zmienne lokalne
+    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)  # rysujemy pusty labirynt od nowa
+    ax.plot(path_x, path_y, lw=2, color='red')  # dodajemy całą wygenerowaną trasę
 
     """Ustawienia osi"""
     plt.xlim(0, 12)
